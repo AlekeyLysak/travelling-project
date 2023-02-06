9a32880581ec934f9b50018da1c9471085295eb9
alexlysak2688@gmail.com <alexlysak2688@gmail.com>
2023-02-07 00:29:25 +0200

    add Header

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import App from 'container/App/App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(<React.StrictMode>
       <App/>
